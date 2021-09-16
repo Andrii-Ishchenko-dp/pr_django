28acab36599490584498af0901ed10491165f0d5
Andrii-Ishchenko-dp <android428901@gmail.com>
2021-09-17 00:09:16 +0300

    Upd3

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -112,8 +112,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import dj_database_url
-import os
-if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
