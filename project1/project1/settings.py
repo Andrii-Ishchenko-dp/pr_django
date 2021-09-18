b0574d52d2f79340091f7b71e3d3fe202e3fa0ad
Andrii-Ishchenko-dp <android428901@gmail.com>
2021-09-18 09:50:06 +0300

    Изменил блок О нас

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -104,13 +106,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_root = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
