062f9e607b1a6de89694b57d8574b30c7fd1ce36
Andrii-Ishchenko-dp <android428901@gmail.com>
2021-09-28 22:54:37 +0300

    commit

--- main/models.py
+++ main/models.py
@@ -1,26 +1,27 @@
 from django.db import models
-import uuid
+
 
 
 class Task(models.Model):
     name_task = models.CharField('Имя', max_length=50)
+    surname = models.CharField('Фамилия', max_length=50, default='')
+    phone_num = models.CharField('Номер телефона', max_length=50, default='')
     title = models.CharField('Страна', max_length= 50)
     title2 = models.CharField('Город', max_length=50)
     task = models.CharField('Улица', max_length=50)
     created_time = models.DateTimeField(auto_now_add=True)
-    surname = models.CharField('Фамилия', max_length= 50, default='')
-    phone_num = models.CharField('Номер телефона', max_length=50, default='')
-    #id=models.CharField(primary_key=True,default=uuid.uuid4, editable=False, max_length=36)
+
+
 
     def __str__(self):
-        return  self.title
+        return  self.name_task
 
     class Meta:
-        verbose_name = 'Задача'
-        verbose_name_plural = 'Задачи'
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
 
-class Review(models.Model):
-    project = models.ForeignKey(Task, on_delete=models.CASCADE, null= True, blank= True)
-    body = models.TextField(null=True, blank=True)
+# class Review(models.Model):
+#     project = models.ForeignKey(Task, on_delete=models.CASCADE, null= True, blank= True)
+#     body = models.TextField(null=True, blank=True)
 
 
