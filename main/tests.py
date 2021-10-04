96184705ad30ad60b6e4af7b6d5c2548cbf844a4
Andrii-Ishchenko-dp <android428901@gmail.com>
2021-10-04 23:37:42 +0300

    commit

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Task
 
-# Create your tests here.
+User = get_user_model()
+
+class TaskTestCases(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(username = "testuser", password="123zxcVBN")
+        self.accaunt = Task.objects.create(
+            name_task = 'Test',
+            surname = 'User',
+            phone_num = '123456',
+            title = 'Test_count',
+            title2 = 'test_city',
+            task = 'test_street'
+        )
